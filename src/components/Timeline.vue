afd623cefba8aabbde0fbe3363f7a6127a539748
Alex <alex.y.kuznetsov@gmail.com>
2021-07-20 00:31:26 +0500

    refactoring

--- src/components/Timeline.vue
+++ src/components/Timeline.vue
@@ -3,14 +3,14 @@
     <div class="timeline_days timeline_upper">
       <div class="timeline_icon timeline_icon_bed"></div>
       <div class="timeline_dot"
-           v-bind:class="{ timeline_dot_highlited: !day.onCouch}"
+           v-bind:class="{ timeline_dot_highlited: !day.onCouch }"
            v-for="day in dayCounter" v-bind:key="day.id">
       </div>
     </div>
     <div class="timeline_days timeline_lower">
       <div class="timeline_icon timeline_icon_couch"></div>
       <div class="timeline_dot"
-           v-bind:class="{ timeline_dot_highlited: day.onCouch}"
+           v-bind:class="{ timeline_dot_highlited: day.onCouch }"
            v-for="day in dayCounter" v-bind:key="day.id">
       </div>
     </div>
