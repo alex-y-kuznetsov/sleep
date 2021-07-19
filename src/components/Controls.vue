744cf73f4e37023744ab0f02b74df05e41b064e0
Alex <alex.y.kuznetsov@gmail.com>
2021-07-19 16:52:41 +0500

    percentage bar text

--- src/components/Controls.vue
+++ src/components/Controls.vue
@@ -2,13 +2,22 @@
   <div class="controls">
     <button class="button button_reset" v-on:click.stop="resetDaysOnCouch">Reset</button>
     <button class="button button_commit">Commit</button>
-    <button class="button button_commit" v-on:click.stop="togglePercentBar">Show Bar</button>
+    <button class="button button_commit" v-on:click.stop="togglePercentBar">
+      {{ isBarShown ? 'Hide Bar' : 'Show Bar' }}
+    </button>
     <button class="button button_commit">Show Timeline</button>
   </div>
 </template>
 
 <script>
+import { mapState } from 'vuex';
+
 export default {
+  computed: {
+    ...mapState([
+      'isBarShown'
+    ])
+  },
   methods: {
     resetDaysOnCouch () {
       this.$store.commit('clearDaysOnCouch');
