0379d36adebb617898acfabb539f40d331ea1987
Alex <alex.y.kuznetsov@gmail.com>
2021-07-19 22:24:08 +0500

    timeline module

--- src/components/Timeline.vue
+++ src/components/Timeline.vue
@@ -1,12 +1,30 @@
 <template>
   <section class="section timeline">
-    <div class="timeline_days timeline_upper"></div>
-    <div class="timeline_days timeline_lower"></div>
+    <div class="timeline_days timeline_upper">
+      <div class="timeline_icon timeline_icon_bed"></div>
+      <div class="timeline_dot"
+           v-bind:class="{ timeline_dot_highlited: !day.onCouch}"
+           v-for="day in dayCounter" v-bind:key="day.id">
+      </div>
+    </div>
+    <div class="timeline_days timeline_lower">
+      <div class="timeline_icon timeline_icon_couch"></div>
+      <div class="timeline_dot"
+           v-bind:class="{ timeline_dot_highlited: day.onCouch}"
+           v-for="day in dayCounter" v-bind:key="day.id">
+      </div>
+    </div>
   </section>
 </template>
 
 <script>
-export default {
+import { mapState } from 'vuex';
 
+export default {
+  computed: {
+    ...mapState([
+      'dayCounter'
+    ])
+  }
 }
 </script>
