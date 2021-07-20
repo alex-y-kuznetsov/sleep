dec8a01f9d68f95c69f1878a3d159c93c4f22524
Alex <alex.y.kuznetsov@gmail.com>
2021-07-20 22:51:36 +0500

    mobile controls + button names

--- src/components/Controls.vue
+++ src/components/Controls.vue
@@ -1,11 +1,13 @@
 <template>
   <section class="section controls">
-    <button class="button button_reset" v-on:click.stop="resetDaysOnCouch">Reset</button>
+    <button class="button button_reset" v-on:click.stop="resetDaysOnCouch">Clear</button>
     <button class="button button_commit" v-on:click.stop="saveDataToStorage">Commit</button>
-    <button class="button button_commit" v-on:click.stop="toggleSection('toggleSection', 'isBarShown')">
+    <button class="button button_bar" v-on:click.stop="toggleSection('toggleSection', 'isBarShown')"
+            v-bind:class="{ 'toggled' : isBarShown }">
       {{ isBarShown ? 'Hide Bar' : 'Show Bar' }}
     </button>
-    <button class="button button_commit" v-on:click.stop="toggleSection('toggleSection', 'isTimelineShown')">
+    <button class="button button_timeline" v-on:click.stop="toggleSection('toggleSection', 'isTimelineShown')"
+            v-bind:class="{ 'toggled' : isTimelineShown }">
       {{ isTimelineShown ? 'Hide Timeline' : 'Show Timeline' }}
     </button>
   </section>
